4323e94d5070eb564a614ea4a1396b8fb881daa3
murillojoaquim <murillojalves@hotmail.com>
2021-08-29 19:43:53 -0300

    Adiciona os comandos

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá usuário, eu me chamo Murillo!");
+            Console.ReadLine();
+
+            Console.WriteLine("Agora me diga, qual o seu nome?");
+            string Nome = Console.ReadLine();
+
+            Console.WriteLine($"É um prazer conhece-lo {Nome}");
+        }
+    }
+}
